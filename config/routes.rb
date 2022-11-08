47faa86458ea81e89bddd13a63be183f9bd3e18b
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-11-08 17:39:51 +0900

    finish destory

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #root
+  root to: 'articles#index'
+  #all
+  get 'articles', to: 'articles#index', as: :articles
+  #create
+  get '/articles/new', to: 'articles#new', as: :new_article
+  post '/articles', to: 'articles#create'
+  #show
+  get 'articles/:id', to: 'articles#show', as: :article
+  #edit/update
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  patch 'articles/:id', to: 'articles#update'
+  #delete
+  delete 'articles/:id', to: 'articles#destroy'
 end
